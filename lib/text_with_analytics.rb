925350079129acd5a38323240b4894ba6d787300
Keith Webber <keith.e.webber@gmail.com>
2014-03-16 19:58:46 -0400

    rubocop and sandi meter say good to go

--- lib/text_with_analytics.rb
+++ lib/text_with_analytics.rb
@@ -1,42 +1,36 @@
-
 class TextWithAnalytics
-  def initialize(text='')
+# standard string in,enhanced to preform analytics
+  def initialize(text = '')
     @text = text
   end
-
-  #O(n^2) -- need to refactor
+# need to refactor
   def word_frequency
-    freq=Hash.new(0)
-    @text.split(' ').each {|word| freq[word] += 1}
+    freq = Hash.new(0)
+    @text.split(' ').each { |word| freq[word] += 1 }
     freq
   end
 
-  #o(n)-- need to refactor
+  # o(n)-- need to refactor
   def letter_frequency
-    freq=Hash.new(0)
-    @text.each_char { |char| (freq[char] += 1 ) if char.match(/[a-zA-Z]/)}
+    freq = Hash.new(0)
+    @text.each_char { |char| (freq[char] += 1) if char.match(/[a-zA-Z]/) }
     freq
   end
 
-  #o(n) -- need to refactor
+  # o(n) -- need to refactor
   def symbol_frequency
-    freq=Hash.new(0)
-    @text.scan(%r{[^a-zA-Z0-9]}).each {|word| freq[word] += 1}
+    freq = Hash.new(0)
+    @text.scan(/[^a-zA-Z0-9]/).each { |word| freq[word] += 1 }
     freq
   end
 
   def top_three
-   word_frequency.sort_by { |word, count | count}.pop(3).map!{|array| array[0]}.reverse
+    tmp = word_frequency.sort_by { |word, count | count }.pop(3)
+    tmp.map! { |array| array[0] }.reverse
   end
 
   def bottom_three
-   word_frequency.sort_by { |word, count | [count,word]}.shift(3).map!{|array| array[0]}
+    tmp = word_frequency.sort_by { |word, count | [count, word] }.shift(3)
+    tmp.map! { |array| array[0] }
   end
-
-
-
-
-
-
-
 end
--- spec/text_with_analytics_spec.rb
+++ spec/text_with_analytics_spec.rb
@@ -3,39 +3,42 @@ require_relative '../lib/text_with_analytics'
 
 describe TextWithAnalytics do
 
-  let(:test){TextWithAnalytics.new(filler_text)}
-  let(:filler_text){"lorem ipsum ipsum"}
+  let(:test) { TextWithAnalytics.new(filler_text) }
+  let(:filler_text) { 'lorem ipsum ipsum' }
 
-  let(:smaller){TextWithAnalytics.new(small_text)}
-  let(:small_text){"woot Ruby rUles!"}
+  let(:smaller) { TextWithAnalytics.new(small_text) }
+  let(:small_text) { 'woot Ruby rUles!' }
 
-  let(:big){TextWithAnalytics.new(big_text)}
-  let(:big_text){"This This    a  paragraph without duplication is is is execpt This the frist a letters This zebra apple crazy"}
+  let(:big) { TextWithAnalytics.new(big_text) }
+  let(:big_text) do 'This This    a  paragraph without
+                    duplication is is is execpt
+                    This the frist a letters
+                    This zebra apple crazy'
+                 end
 
-
-
-  it "Number of each word" do
-    expect(test.word_frequency).to eq({'lorem' => 1, 'ipsum' => 2})
+  it 'Number of each word' do
+    expect(test.word_frequency).to eq('lorem' => 1, 'ipsum' => 2)
   end
 
-  it "Number of each letter" do
-    expect(smaller.letter_frequency).to eq({'w'=>1,'o'=>2,'t'=>1, 'R'=>1,'u'=>1,'b'=>1,'y'=>1,'r'=>1,'U'=>1,'l'=>1, 'e'=>1, 's'=>1})
+  it 'Number of each letter' do
+    expect(smaller.letter_frequency).to eq('w' => 1, 'o' => 2, 't' => 1,
+                                           'R' => 1, 'u' => 1, 'b' => 1,
+                                           'y' => 1, 'r' => 1, 'U' => 1,
+                                           'l' => 1, 'e' => 1, 's' => 1)
   end
 
-  it "Number of each symbol (any non-letter and non-digit character, excluding white space)" do
-    expect(smaller.symbol_frequency).to eq({' '=> 2,'!'=>1})
+  it 'Number of each symbol' do
+    expect(smaller.symbol_frequency).to eq(' ' => 2, '!' => 1)
   end
 
-
-  it "Top three most common words" do
-    expect(big.top_three).to eq(["This", "is", "a"])
+  it 'Top three most common words' do
+    expect(big.top_three).to eq(%w(This is a))
   end
 
-  it "Top three most common letters" do
-    expect(big.bottom_three).to eq(["apple", "crazy", "duplication"])
+  it 'Top three most common letters' do
+    expect(big.bottom_three).to eq(%w(apple crazy duplication))
   end
 
-
-  it "Number of words only used once"
-  it "All letters not used in the stringr"
+  it 'Number of words only used once'
+  it 'All letters not used in the string'
 end
